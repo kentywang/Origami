9ec2ce9320277881ac6343b109ebb6a757a568e9
Kenty Wang <kentymail@gmail.com>
2017-09-20 23:59:00 -0400

    Build menu item and start on settings pane

--- Origami/AppDelegate.swift
+++ Origami/AppDelegate.swift
@@ -10,17 +10,35 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarItemImage")
+            button.action = #selector(woof(_:))
+        }
+        
+        constructMenu()
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @objc func woof(_ sender: Any?) {
+        print("-_-")
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Bark", action: #selector(AppDelegate.woof(_:)), keyEquivalent: "b"))
+        menu.addItem(NSMenuItem(title: "Bark", action: #selector(AppDelegate.woof(_:)), keyEquivalent: "b"))
+        menu.addItem(NSMenuItem(title: "Bark", action: #selector(AppDelegate.woof(_:)), keyEquivalent: "b"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
 }
-
