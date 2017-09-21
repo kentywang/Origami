b20f57165267095faf29d59bd11a2d947584e2c1
Kenty Wang <kentymail@gmail.com>
2017-09-20 22:43:14 -0400

    Initial Commit

---
+++ Origami/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Origami
+//
+//  Created by Kenty Wang on 9/20/17.
+//  Copyright © 2017 Kenty Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Origami/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Origami
+//
+//  Created by Kenty Wang on 9/20/17.
+//  Copyright © 2017 Kenty Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
